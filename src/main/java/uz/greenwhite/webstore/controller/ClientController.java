3bb56830d2008d2563295e00cbe7a1c354d85fde
Lazizkhan1 <lazizkhanhadjayev@gmail.com>
2023-09-20 09:28:03 -0700

    ClientController

--- src/main/java/uz/greenwhite/webstore/controller/ClientController.java
+++ src/main/java/uz/greenwhite/webstore/controller/ClientController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class TestController {
+public class ClientController {
     @GetMapping()
     public String indexController() {
         return "index";
