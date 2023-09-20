1e3f81cee53530253d3c796a0cb08fbfc35af791
Abdullohasas <abdulloh4you@gmail.com>
2023-09-20 21:53:32 +0500

    Category entity qo'shildi

---
+++ src/main/java/uz/greenwhite/webstore/Entity/Category.java
@@ -0,0 +1,22 @@
+package uz.greenwhite.webstore.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "category")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Integer categoryId;
+
+    private String category_name;
+
+}
