4655f2d6db46ffd6d57395cb3cc30d24c4077620
shamsiddin <nasimovshamsiddin97@gmail.com>
2023-09-21 19:26:19 +0500

    OrderItem

--- src/main/java/uz/greenwhite/webstore/entity/OrderItem.java
+++ src/main/java/uz/greenwhite/webstore/entity/OrderItem.java
@@ -13,14 +13,16 @@ public class OrderItem {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long orderItemId;
+    private Long order_itemId;
 
     private Integer quantity;
 
     @ManyToOne
+    @JoinColumn(name = "order_id")
     private Orders orders;
 
     @ManyToOne
+    @JoinColumn(name = "product_id")
     private Product product;
 
 }
