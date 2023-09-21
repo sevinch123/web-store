73f7dc36dc86ae6ffc4cbc57cefaae6cbee92e51
akmal-2004 <akmallatifov2004@gmail.com>
2023-09-21 14:05:03 +0500

    columns are removed for CompanyDetails entity

--- src/main/java/uz/greenwhite/webstore/entity/CompanyDetails.java
+++ src/main/java/uz/greenwhite/webstore/entity/CompanyDetails.java
@@ -15,31 +15,23 @@ public class CompanyDetails {
     @Column(name = "company_details_id", nullable = false)
     private Long companyDetailsId;
 
-    @Column(name = "company_name")
     private String companyName;
 
-    @Column(name = "logo_path")
     private String logoPath;
 
-    @Column(name = "phone_1")
     private String phone1;
 
-    @Column(name = "phone_2")
     private String phone2;
 
-    @Column(name = "email")
     private String email;
 
-    @Column(name = "instagram_url")
     private String instagramUrl;
 
-    @Column(name = "telegramUrl")
     private String telegramUrl;
 
-    @Column(name = "address", columnDefinition = "TEXT")
+    @Column(columnDefinition = "TEXT")
     private String address;
 
-    @Column(name = "location_url")
     private String locationUrl;
 
 }
