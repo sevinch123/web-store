359ff9a4ad8a8b138e361800d5976c0a6d6fb08c
Akbarov90 <ilyasovich706@gmail.com>
2023-09-27 22:43:01 +0500

    finish user category scroll

--- src/main/java/uz/greenwhite/webstore/controller/client/ClientController.java
+++ src/main/java/uz/greenwhite/webstore/controller/client/ClientController.java
@@ -1,12 +1,27 @@
 package uz.greenwhite.webstore.controller.client;
 
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import uz.greenwhite.webstore.entity.Category;
+import uz.greenwhite.webstore.service.CategoryService;
+import org.springframework.ui.Model;
+import org.springframework.data.domain.Pageable;
 
 @Controller
 public class ClientController {
-    @GetMapping("/")
-    public String indexController() {
+    private final CategoryService service;
+
+    public ClientController(CategoryService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public String list(Model model, Pageable pageable) {
+        Page<Category> page = service.getAll(pageable);
+        long elements = page.getTotalElements();
+        model.addAttribute("categories", page);
+        model.addAttribute("elements", elements);
         return "index";
     }
     @GetMapping("/cart")
@@ -29,5 +44,5 @@ public class ClientController {
     public String checkoutController() {
         return "checkout";
     }
-    
+
 }
