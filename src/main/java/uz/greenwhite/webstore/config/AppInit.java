69e93c28b590b6f82e8886ce835f569010088667
Sevinch <jamulyamuminova@gmail.com>
2023-10-03 21:04:24 +0300

    'fix'

--- src/main/java/uz/greenwhite/webstore/config/AppInit.java
+++ src/main/java/uz/greenwhite/webstore/config/AppInit.java
@@ -1,13 +1,18 @@
 package uz.greenwhite.webstore.config;
 
 import lombok.AllArgsConstructor;
+
+import java.io.File;
+
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-import uz.greenwhite.webstore.entity.Category;
+import uz.greenwhite.webstore.entity.CompanyDetails;
 import uz.greenwhite.webstore.entity.Users;
 import uz.greenwhite.webstore.enums.UserRole;
+import uz.greenwhite.webstore.repository.CompanyDetailsRepository;
+import uz.greenwhite.webstore.entity.Category;
 import uz.greenwhite.webstore.repository.CategoryRepository;
 import uz.greenwhite.webstore.repository.UserRepository;
 import uz.greenwhite.webstore.service.CategoryService;
@@ -20,10 +25,23 @@ public class AppInit implements ApplicationRunner {
 //
 //    private final UserRepository userRepository;
 //    private final PasswordEncoder encoder;
+
+    private final CompanyDetailsRepository companyRepository;
     private final CategoryService categoryService;
 
     @Override
-    public void run(ApplicationArguments args) throws Exception {
+    public void run(ApplicationArguments args) throws Exception { 
+        CompanyDetails details = new CompanyDetails();
+        details.setCompanyName("Anor");
+        details.setAddress("Komolon");
+        details.setEmail("anor@gmail.com");
+        details.setInstagramUrl("http//anor/market");
+        details.setTelegramUrl("http//anor/market");
+        details.setLocationUrl("http//anor/market");
+        details.setPhone1("128-02-121");
+        details.setPhone2("251-54-254");
+        companyRepository.save(details);
+        
         Category category = new Category();
         category.setCategoryName("Texnika");
         categoryService.save(category);
@@ -31,7 +49,6 @@ public class AppInit implements ApplicationRunner {
         category.setCategoryName("Mebel");
         category.setIsActive(false);
         categoryService.save(category);
-//        new File("FILE").mkdir();
 //        if (userRepository.findByUsername("admin").isEmpty()) {
 //            Users admin = new Users();
 //            admin.setRole(UserRole.MODERATOR);
